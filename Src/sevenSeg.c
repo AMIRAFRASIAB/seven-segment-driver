e0050af21e50797c97bddc05c89adaf715781cf7
AMIR-HOSEIN-BAGHERI <whitewolf97@yahoo.com>
2024-04-04 23:05:44 +0330

    Add All English Letters To Decoder + Refresh API Bug Fixed

--- Src/sevenSeg.c
+++ Src/sevenSeg.c
@@ -24,19 +24,97 @@ static uint8_t __sevenSegDecoder (const char CHAR) {
       ans = 0x66;
       break;
     case '5':
+    case 'S':
+    case 's':
       ans = 0x6D;
       break;
     case '6':
+    case 'B':
+    case 'b':
       ans = 0x7C;
       break;
     case '7':
-      ans = 0x0F;
+      ans = 0x07;
       break;
     case '8':
       ans = 0x7F;
       break;
     case '9':
+    case 'Q':
+    case 'q':
       ans = 0x67;
+      break;
+    case 'A':
+    case 'a':
+      ans = 0x77;
+      break;
+    case 'C':
+    case 'c':
+      ans = 0x39;
+      break;
+    case 'D':
+    case 'd':
+      ans = 0x5E;
+      break;
+    case 'E':
+    case 'e':
+      ans = 0x79;
+      break;
+    case 'F':
+    case 'f':
+      ans = 0x71;
+      break;
+    case 'G':
+    case 'g':
+      ans = 0x3D;
+      break;
+    case 'H':
+    case 'h':
+      ans = 0x76;
+      break;
+    case 'I':
+    case 'i':
+      ans = 0x30;
+      break;
+    case 'J':
+    case 'j':
+      ans = 0x1E;
+      break;
+    case 'L':
+    case 'l':
+      ans = 0x38;
+      break;
+    case 'N':
+    case 'n':
+      ans = 0x54;
+      break;
+    case 'O':
+    case 'o':
+      ans = 0x5C;
+      break;
+    case 'P':
+    case 'p':
+      ans = 0x73;
+      break;
+    case 'R':
+    case 'r':
+      ans = 0x50;
+      break;
+    case 'T':
+    case 't':
+      ans = 0x78;
+      break;
+    case 'U':
+    case 'u':
+      ans = 0x1C;
+      break;
+    case 'V':
+    case 'v':
+      ans = 0x3E;
+      break;
+    case 'Y':
+    case 'y':
+      ans = 0x6E;
       break;
     case ' ':
       ans = 0x00;
@@ -113,11 +191,6 @@ void sevenSegment_refresh (sevenSeg_t* p7seg) {
   if (p7seg->initFlag == false) {
     return;
   }
-  p7seg->refreshCounter++;
-  if (p7seg->refreshCounter >= p7seg->NUM_OF_DIGITS) {
-    p7seg->refreshCounter = 0;
-    p7seg->stack[0] = 0;
-  }
   /* Disable All Common Outputs To Avoid UnWanted Refreshes */
   for (uint8_t i = 0; i < p7seg->NUM_OF_DIGITS; i++) {
     p7seg->writePin(p7seg->COMMONS[i], (p7seg->commonType == pinActiveType_High)? false : true);
@@ -136,6 +209,12 @@ void sevenSegment_refresh (sevenSeg_t* p7seg) {
   }
   /* Enable Single Common Output Pin */
   p7seg->writePin(p7seg->COMMONS[p7seg->refreshCounter], (p7seg->commonType == pinActiveType_High)? true : false);
+  /* Refresh Counter Increment */
+  p7seg->refreshCounter++;
+  if (p7seg->refreshCounter >= p7seg->NUM_OF_DIGITS) {
+    p7seg->refreshCounter = 0;
+    p7seg->stack[0] = 0;
+  }
 }
 //----------------------------------------------------------------------------------------
 
